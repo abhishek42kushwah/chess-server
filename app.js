206b5761e1cdb3651aeff10aa817ef96d8fd0bda
Abhishek kushwah <113005763+abhishek42kushwah@users.noreply.github.com>
2024-08-06 10:26:18 +0530

    "Added socket.io functionality to app.js and created index.ejs view file"

--- app.js
+++ app.js
@@ -10,18 +10,67 @@ const io = socket(server)
 
 const chess = new Chess()
 
-let Player = {}
+let players = {}
 let currentPlayer = "W"
 
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")))
 
+io.on("connection",(uniqueSocket)=>{
+console.log("Connected");
+
+if(!players.white){
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit("playerRole","w")
+}
+else if(!players.black){
+    players.black = uniqueSocket.id; 
+    uniqueSocket.emit("playerRole","b")
+}
+else{
+    uniqueSocket.emit("spectatorRole")
+}
+
+uniqueSocket.on("disconnect",()=>{
+    if(uniqueSocket.id === players.white){
+        delete players.white;
+    }
+    else if(uniqueSocket.id === players.black){
+        delete players.black
+    }
+})
+
+uniqueSocket.on("move",(move)=>{
+    try {
+        if(chess.turn() === "w" && uniqueSocket.id !== players.white) return;
+        if(chess.turn() === "b" && uniqueSocket.id !== players.black) return;
+
+       const  result =   chess.move(move)
+
+       if(result){
+        currentPlayer = chess.turn();
+        io.emit("move",move);
+        io.emit("boardState",chess.fen())
+       }
+       else{
+        console.log("Invalid move", move);
+        uniqueSocket.emit("Invalid move",move);
+       }
+         } catch (error) {
+            console.log(error);
+            uniqueSocket.emit("Invalid move",move)
+            
+    }
+})
+
+
+})
 
 const PORT = 5000
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`
 )})
 
 app.get("/",(req,res)=>{
-    res.send("index");
+    res.render("index",{title:"Chess Game"});
 })
